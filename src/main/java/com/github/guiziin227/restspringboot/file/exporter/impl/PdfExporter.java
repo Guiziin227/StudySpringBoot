0343e7d4eb37fb710ce1c21459efb6a34d7e4c17
Guiziin227 <gh63388@gmail.com>
2025-07-11 19:16:17 -0300

    feat: add QRCodeService and implement exportPerson method in PdfExporter

--- src/main/java/com/github/guiziin227/restspringboot/file/exporter/impl/PdfExporter.java
+++ src/main/java/com/github/guiziin227/restspringboot/file/exporter/impl/PdfExporter.java
@@ -2,24 +2,30 @@ package com.github.guiziin227.restspringboot.file.exporter.impl;
 
 import com.github.guiziin227.restspringboot.dto.PersonDTO;
 import com.github.guiziin227.restspringboot.file.exporter.contract.FileExporter;
+import com.github.guiziin227.restspringboot.service.QRCodeService;
 import net.sf.jasperreports.engine.JasperCompileManager;
 import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @Component
 public class PdfExporter implements FileExporter {
+
+    @Autowired
+    private QRCodeService qrCodeService;
 
 
     @Override
@@ -41,7 +47,44 @@ public class PdfExporter implements FileExporter {
         try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
             JasperExportManager.exportReportToPdfStream(jasperPrint, baos);
             return new ByteArrayResource(baos.toByteArray());
-        }catch (RuntimeException e) {
+        } catch (RuntimeException e) {
+            throw new RuntimeException("Error exporting PDF", e);
+        }
+    }
+
+    @Override
+    public Resource exportPerson(PersonDTO person) throws Exception {
+        InputStream mainStream = getClass().getResourceAsStream("/templates/person.jrxml");
+
+        if (mainStream == null) {
+            throw new RuntimeException("Template file not found");
+        }
+
+        InputStream subStream = getClass().getResourceAsStream("/templates/books.jrxml");
+
+        if (subStream == null) {
+            throw new RuntimeException("Template file not found");
+        }
+
+        JasperReport jasperReport = JasperCompileManager.compileReport(mainStream);
+        JasperReport subReport = JasperCompileManager.compileReport(subStream);
+
+        InputStream qrCodeStream = qrCodeService.generateQRCode(person.getProfileUrl(), 200, 200);
+
+        JRBeanCollectionDataSource subSource = new JRBeanCollectionDataSource(person.getBooks());
+        Map<String, Object> parameters = new HashMap<String, Object>();
+        parameters.put("SUB_REPORT_DATA_SOURCE", subSource);
+        parameters.put("BOOK_SUB_REPORT", subReport);
+        parameters.put("QR_CODEIMAGE", qrCodeStream);
+
+        JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(Collections.singletonList(person));
+
+
+        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, dataSource);
+        try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+            JasperExportManager.exportReportToPdfStream(jasperPrint, baos);
+            return new ByteArrayResource(baos.toByteArray());
+        } catch (RuntimeException e) {
             throw new RuntimeException("Error exporting PDF", e);
         }
     }
---
+++ src/main/java/com/github/guiziin227/restspringboot/service/QRCodeService.java
@@ -0,0 +1,28 @@
+package com.github.guiziin227.restspringboot.service;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import org.springframework.stereotype.Service;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+@Service
+public class QRCodeService {
+    public InputStream generateQRCode(String profileUrl, int w, int h) throws WriterException, IOException {
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+
+        BitMatrix bitMatrix = qrCodeWriter.encode(profileUrl, BarcodeFormat.QR_CODE, w, h);
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+        MatrixToImageWriter.writeToStream(bitMatrix, "PNG", outputStream);
+
+        return new ByteArrayInputStream(outputStream.toByteArray());
+    }
+}
