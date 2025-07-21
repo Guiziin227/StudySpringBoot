9f12ebe7a261dd1d536925af954f82c191618987
Guiziin227 <gh63388@gmail.com>
2025-07-21 20:10:35 -0300

    feat: add JwtTokenFilter

---
+++ src/main/java/com/github/guiziin227/restspringboot/jwt/JwtTokenFilter.java
@@ -0,0 +1,42 @@
+package com.github.guiziin227.restspringboot.jwt;
+
+import io.micrometer.common.util.StringUtils;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.filter.GenericFilterBean;
+
+import java.io.IOException;
+
+public class JwtTokenFilter extends GenericFilterBean {
+
+    private JwtTokenProvider tokenProvider;
+
+    public JwtTokenFilter(JwtTokenProvider tokenProvider) {
+        this.tokenProvider = tokenProvider;
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain filterChain)
+            throws IOException, ServletException {
+
+        var token = tokenProvider.resolveToken((HttpServletRequest) req);
+
+        if (StringUtils.isNotBlank(token) && tokenProvider.validateToken(token)) {
+
+            Authentication auth = tokenProvider.getAuthentication(token);
+            if (auth != null) {
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            }
+
+        }
+
+        filterChain.doFilter(req, res);
+    }
+}
