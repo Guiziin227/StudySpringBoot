fecbf5350e8ee0fa384b44f0746904ad54aa63a8
Guiziin227 <gh63388@gmail.com>
2025-07-22 20:08:53 -0300

    feat: enhance PersonServiceTest with pagination and link validation

---
+++ src/main/java/com/github/guiziin227/restspringboot/service/AuthService.java
@@ -0,0 +1,45 @@
+package com.github.guiziin227.restspringboot.service;
+
+import com.github.guiziin227.restspringboot.dto.security.AccountCredentialsDTO;
+import com.github.guiziin227.restspringboot.dto.security.TokenDTO;
+import com.github.guiziin227.restspringboot.jwt.JwtTokenProvider;
+import com.github.guiziin227.restspringboot.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public ResponseEntity<TokenDTO> signIn(AccountCredentialsDTO credentials) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        credentials.getUsername(),
+                        credentials.getPassword()
+                )
+        );
+
+        var user = userRepository.findByUsername(credentials.getUsername());
+
+        if (user == null) throw new UsernameNotFoundException("User not found " + credentials.getUsername());
+
+        var tokenResponse = jwtTokenProvider.createAccessToken(
+                user.getUsername(),
+                user.getRoles()
+        );
+
+        return ResponseEntity.ok(tokenResponse);
+    }
+}
