2e341289a369a221e98c7ddcee254a36cbed5e81
Guiziin227 <gh63388@gmail.com>
2025-07-23 19:48:02 -0300

    refactor: remove unused import and clean up UserRepository

--- src/main/java/com/github/guiziin227/restspringboot/repository/UserRepository.java
+++ src/main/java/com/github/guiziin227/restspringboot/repository/UserRepository.java
@@ -2,12 +2,12 @@ package com.github.guiziin227.restspringboot.repository;
 
 import com.github.guiziin227.restspringboot.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    @Modifying
+
     @Query("SELECT u FROM User u WHERE u.username = :username")
     User findByUsername(@Param("username") String username);
+
 }
