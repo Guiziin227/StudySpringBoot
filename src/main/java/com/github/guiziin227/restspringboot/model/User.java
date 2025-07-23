c52b2f91128b4262561047dee2c963d7659f3e4d
Guiziin227 <gh63388@gmail.com>
2025-07-23 19:47:54 -0300

    fix: correct table name for user permissions in User entity

--- src/main/java/com/github/guiziin227/restspringboot/model/User.java
+++ src/main/java/com/github/guiziin227/restspringboot/model/User.java
@@ -59,7 +59,7 @@ public class User implements UserDetails, Serializable {
     private Boolean enabled;
 
     @ManyToMany(fetch = FetchType.EAGER)
-    @JoinTable(name = "user_permissions",
+    @JoinTable(name = "user_permission",
             joinColumns = {@JoinColumn(name = "id_user")},
             inverseJoinColumns = {@JoinColumn(name = "id_permission")})
     private List<Permission> permissions;
