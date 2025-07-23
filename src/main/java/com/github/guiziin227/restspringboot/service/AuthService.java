37e8d75939e6cdd73003899d1ba943a2b9fc3a6b
Guiziin227 <gh63388@gmail.com>
2025-07-23 20:14:37 -0300

    feat: add refreshToken method to AuthService for token renewal

--- src/main/java/com/github/guiziin227/restspringboot/service/AuthService.java
+++ src/main/java/com/github/guiziin227/restspringboot/service/AuthService.java
@@ -4,6 +4,7 @@ import com.github.guiziin227.restspringboot.dto.security.AccountCredentialsDTO;
 import com.github.guiziin227.restspringboot.dto.security.TokenDTO;
 import com.github.guiziin227.restspringboot.jwt.JwtTokenProvider;
 import com.github.guiziin227.restspringboot.repository.UserRepository;
+import org.antlr.v4.runtime.Token;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -41,5 +42,19 @@ public class AuthService {
         );
 
         return ResponseEntity.ok(tokenResponse);
+    }
+
+
+    public ResponseEntity<TokenDTO> refreshToken(String username, String refreshToken) {
+        var user = userRepository.findByUsername(username);
+        TokenDTO tokenDTO;
+
+        if (user != null){
+            tokenDTO = jwtTokenProvider.refreshToken(refreshToken);
+        } else {
+            throw new UsernameNotFoundException("User not found " + username);
+        }
+
+        return ResponseEntity.ok(tokenDTO);
     }
 }
