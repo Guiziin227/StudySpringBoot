6136d9bb8110bce2a9eb4f57b000ab61df74cdad
Guiziin227 <gh63388@gmail.com>
2025-07-23 19:48:32 -0300

    feat: add AuthController for authentication endpoints

---
+++ src/main/java/com/github/guiziin227/restspringboot/controller/AuthController.java
@@ -0,0 +1,45 @@
+package com.github.guiziin227.restspringboot.controller;
+
+import com.github.guiziin227.restspringboot.dto.security.AccountCredentialsDTO;
+import com.github.guiziin227.restspringboot.dto.security.TokenDTO;
+import com.github.guiziin227.restspringboot.service.AuthService;
+import io.micrometer.common.util.StringUtils;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "Authentication endpoints")
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
+    @Operation(summary = "Sign in to the application")
+    @PostMapping("/signin")
+    public ResponseEntity<TokenDTO> signin(@RequestBody AccountCredentialsDTO credentials) {
+
+        if (credentials == null
+                || StringUtils.isEmpty(credentials.getUsername())
+                || StringUtils.isEmpty(credentials.getPassword()))
+        {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        var token = authService.signIn(credentials);
+
+        if (token == null || token.getBody() == null) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        return ResponseEntity.ok().body(token.getBody());
+    }
+
+}
