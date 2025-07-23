6aab5b5fbd1869b7dfdbf4469860d54b41e10518
Guiziin227 <gh63388@gmail.com>
2025-07-23 20:14:29 -0300

    feat: add refreshToken endpoint to AuthController for token renewal

--- src/main/java/com/github/guiziin227/restspringboot/controller/AuthController.java
+++ src/main/java/com/github/guiziin227/restspringboot/controller/AuthController.java
@@ -9,8 +9,11 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -40,6 +43,26 @@ public class AuthController {
         }
 
         return ResponseEntity.ok().body(token.getBody());
+    }
+
+    @Operation(summary = "Refresh the authentication token")
+    @PutMapping("/refresh/{username}")
+    public ResponseEntity<?> refreshToken(@PathVariable("username") String username,
+                                                 @RequestHeader("Authorization") String refreshToken) {
+
+        if (parametersAreInvalid(username, refreshToken)) return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+
+
+        var token = authService.refreshToken(username, refreshToken);
+
+        if (token == null || token.getBody() == null) return ResponseEntity.badRequest().build();
+
+        return ResponseEntity.ok().body(token.getBody());
+    }
+
+    private boolean parametersAreInvalid(String username, String refreshToken) {
+        return StringUtils.isBlank(username) || StringUtils.isBlank(refreshToken)
+                || !refreshToken.startsWith("Bearer ");
     }
 
 }
